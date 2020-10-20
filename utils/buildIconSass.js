1b654121fc103386ca8a630914bad043d9c3610b
Jordan Byrd <jabyrd3@gmail.com>
2020-10-20 15:00:18 -0400

    try again

--- utils/buildIconSass.js
+++ utils/buildIconSass.js
@@ -6,7 +6,7 @@ const files = fs.readdirSync('./assets/svg')
     f.split('.svg')[0]);
 
 fs
-  .writeFileSync(`./scss/icons.css`,
+  .writeFileSync(`./scss/icons.scss`,
     files
       .filter(f=>f)
       .filter(f=>f[0]!=='.')
@@ -15,35 +15,7 @@ fs
           String
             .fromCharCode(0xEA01 + (i))
           }' !global`)
-      .join('\n') + `
-            @font-face {
-            font-family: "icons";
-            src: url("/eot");
-            src: url("iconfont.eot?#iefix") format("embedded-opentype"),
-                 url("iconfont.woff") format("woff"),
-                 url("iconfont.ttf") format("truetype"),
-                 url("iconfont.svg#iconfont") format("svg");
-            font-weight: normal;
-            font-style: normal;
-          }
-          .icon::before{
-            vertical-align: middle;
-            font-family: "icons"!important;
-            display: inline-block;
-            font-style: normal;
-            font-weight: normal;
-            font-variant: normal;
-            padding: 0 4px;
-            line-height: 1;
-            font-size: 18px;
-            text-decoration: inherit;
-            text-rendering: optimizeLegibility;
-            text-transform: none;
-            -moz-osx-font-smoothing: grayscale;
-            -webkit-font-smoothing: antialiased;
-            font-smoothing: antialiased;
-          }
-        `, 'utf8', (e, done)=>{
+      .join('\n'), 'utf8', (e, done)=>{
         if (e){
           console.log(e);
         }
